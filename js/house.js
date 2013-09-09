1fcc1a261f896fa5928b4c09b163b731c51700c8
lushh <lushouheng@gmail.com>
2013-09-09 18:17:50 +0800

    aa

---
+++ js/house.js
@@ -0,0 +1,20 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Administrator
+ * Date: 13-9-5
+ * Time: 上午11:40
+ * To change this template use File | Settings | File Templates.
+ */
+function PageObj(){
+
+}
+
+PageObj.prototype.init = function(){
+
+}
+
+$(function(){
+    var pageobj = new PageObj();
+    pageobj.init();
+})
+
