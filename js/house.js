4f7c0e4847433171bdc093a9f978a6c0fa16fca4
lushh <lushouheng@gmail.com>
2013-09-10 10:44:36 +0800

    11111

--- js/house.js
+++ js/house.js
@@ -10,7 +10,19 @@ function PageObj(){
 }
 
 PageObj.prototype.init = function(){
+    $(".goods li").click(function(){
+        $(".mask").show();
+        $(".modal").show();
+        var id = $(this).find("p").html();
+        var price = $(this).find(".priceNow").html();
+        $("#goodsId").val(id);
+        $("#goodsPrice").val(price);
+    })
 
+    $(".close").click(function(){
+        $(".mask").hide();
+        $(".modal").hide();
+    })
 }
 
 $(function(){
